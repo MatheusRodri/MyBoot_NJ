f50759f0cf72fae30173546cc8e070e0082f79b1
MatheusRodri <mathclash25@gmail.com>
2021-11-05 16:56:47 -0300

    add second menu in english and portuguese

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,6 +1,7 @@
 const env = require('../../.env')
 const Telegraf = require('telegraf')
 const Markup = require('telegraf/markup')
+const Extra = require('telegraf/extra')
 const bot = new Telegraf(env.token)
 
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 const env = require('../.env')
 const Telegraf = require('telegraf')
 const Markup = require('telegraf/markup')
+const Extra = require('telegraf/extra')
 const bot = new Telegraf(env.token)
-const {teste} = require('./functions/index')
+//const {teste} = require('./functions/index')
 
 
-const tecladoCarne = Markup.keyboard([ 
-    ['🐷 Porco', '🐮 Vaca', '🐑 Carneiro'],
-    ['🐔 Galinha', '🐣 Eu como é ovo'],
-    ['🐟 Peixe', '🐙 Frutos do mar'],
-    ['🍄 Eu sou vegetariano']
+const infoKeyboardPt = Markup.keyboard([ 
+    ['😺 Meu GitHub','💼 Meu Linkedin'],
+    ['📱 Meu numero de telefone','✉️ Meu email']
+]).resize().extra()
+const infoKeyboardEn = Markup.keyboard([ 
+    ['😺 My GitHub','💼 My Linkedin'],
+    ['📱 My phone number','✉️ My email']
 ]).resize().extra()
 
+const optionsKeyboardBr =  Markup.keyboard([
+        ['Sim','não']
+]).resize().extra()
+const optionsKeyboardEn =  Markup.keyboard([
+        ['Yes','No']
+]).resize().extra()
+
+
+const language = '';
 bot.start(ctx => {
     ctx.reply("Escolha um idioma/Which a idiom:",
         Markup.keyboard(['🇧🇷 Português', '🇺🇸 English']).resize().oneTime().extra())
@@ -19,16 +31,51 @@ bot.start(ctx => {
 
 bot.hears('🇧🇷 Português', ctx => {
     const from = ctx.update.message.from
+    language = 'BR';
     ctx.reply(`Seja bem vindo, ${from.first_name}!
-              Eu sou o MathBot, bot desenvolvido pelo Matheus, será um prazer em te ajudar`)
-    ctx.reply(teste("pt-br"))    
+              Eu sou o MathBot, bot desenvolvido pelo Matheus, Em que posso ajudar?`,infoKeyboardPt)         
 })
-
 
 bot.hears('🇺🇸 English', ctx => {
     const from = ctx.update.message.from
     ctx.reply(`Welcome, ${from.first_name}!
-              I'm the MathBot, bot developed by Matheus, it will be a pleasure to help you`)
+              I'm the MathBot, bot developed by Matheus, it will be a pleasure to help you`,infoKeyboardEn)
+})
+
+bot.hears('😺 Meu GitHub', ctx => {
+    ctx.replyWithMarkdown('[Github](https://github.com/MatheusRodri)')
+    ctx.reply("Posso te ajudar em algo a mais ?",optionsKeyboardBr)
+})
+bot.hears('😺 My GitHub',ctx => {
+    ctx.replyWithMarkdown('[Github](https://github.com/MatheusRodri)')
+    ctx.reply('Can I help you with something else?',optionsKeyboardEn)
+})
+
+bot.hears('💼 Meu Linkedin', ctx => {
+    ctx.replyWithMarkdown('[Linkedin](https://www.linkedin.com/in/matheus-rodri/)')
+    ctx.reply("Posso te ajudar em algo a mais ?",optionsKeyboardBr)
+})
+bot.hears('💼 My Linkedin',ctx => {
+    ctx.replyWithMarkdown('[Linkedin](https://www.linkedin.com/in/matheus-rodri/)')
+    ctx.reply('Can I help you with something else?',optionsKeyboardEn)
+})
+
+bot.hears('📱 Meu numero de telefone', ctx => {
+    ctx.reply(env.myNumber)
+    ctx.reply("Posso te ajudar em algo a mais ?",optionsKeyboardBr)
+})
+bot.hears('📱 My phone number',ctx => {
+    ctx.reply(env.myNumber)
+    ctx.reply('Can I help you with something else?',optionsKeyboardEn)
+})
+
+bot.hears('✉️ Meu email', ctx => {
+    ctx.reply(env.myEmail)
+    ctx.reply("Posso te ajudar em algo a mais ?",optionsKeyboardBr)
+})
+bot.hears('✉️ My email',ctx => {
+    ctx.reply(env.myEmail)
+    ctx.reply('Can I help you with something else?',optionsKeyboardEn)
 })
 
 bot.startPolling()
